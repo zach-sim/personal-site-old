42e711e17d27a4a27644b3b01f9c7439576ddcdb
Zach Sim <simjza@gmail.com>
2018-01-13 12:43:34 +1300

    Add & configure webpack-bundle-analyser

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,20 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = function override(config, env) {
   if (env === 'production') {
     delete config.devtool;
   }
+
+  config.plugins = (config.plugins || []).concat([
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      generateStatsFile: true,
+      reportFilename: '../report.html',
+      statsFilename: '../stats.json',
+    }),
+  ]);
+
   // do stuff with the webpack config...
   return config;
 };
