dfb067fb49c03269cdc24d25723aaa1e8e3b0253
Zach Sim <simjza@gmail.com>
2018-04-28 23:19:52 +1200

    Reconfigure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,64 +3,63 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-const dev = process.env.NODE_ENV === 'development' || process.env.WEBPACK_SERVE !== undefined;
-
-module.exports = {
-  mode: dev ? 'development' : 'production',
-  entry: './src/index.js',
-  output: {
-    path: __dirname + '/build',
-    publicPath: '/'
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      automaticNameDelimiter: '.'
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        type: 'javascript/auto',
-        test: /\.json$/,
-        use: './loaders/json'
-      },
-      {
-        type: 'javascript/auto',
-        test: /\.json.gz$/,
-        use: ['./loaders/json', 'gzip-loader']
-      },
-      {
-        test: /\.md$/,
-        use: './loaders/markdown'
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      },
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new webpack.DefinePlugin({
-      PATH_PREFIX: JSON.stringify(''),
-      DEVELOPMENT: JSON.stringify(dev)
-    }),
-    new HtmlWebpackPlugin({ title: 'Zach Sim' }),
-    // new BundleAnalyzerPlugin({
-    //   analyzerMode: 'static',
-    //   openAnalyzer: false,
-    //   generateStatsFile: true,
-    // }),
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-}
+module.exports = (env, argv) => {
+  return ({
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/build',
+      publicPath: '/'
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+        automaticNameDelimiter: '.'
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          type: 'javascript/auto',
+          test: /\.json$/,
+          use: './loaders/json'
+        },
+        {
+          type: 'javascript/auto',
+          test: /\.json.gz$/,
+          use: ['./loaders/json', 'gzip-loader']
+        },
+        {
+          test: /\.md$/,
+          use: './loaders/markdown'
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        },
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new webpack.DefinePlugin({
+        PATH_PREFIX: JSON.stringify(''),
+        DEVELOPMENT: JSON.stringify(argv.mode === 'development')
+      }),
+      new HtmlWebpackPlugin({ title: 'Zach Sim' }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false,
+        reportFilename: `../report-${argv.mode}.html`,
+      }),
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  })
+};
